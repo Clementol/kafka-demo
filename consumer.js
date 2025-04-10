9d09b5ffec0ff82465eafcda1f7ed6d5d5774bbc
MAC <mac@MACs-MacBook-Pro-2.local>
2025-04-10 10:33:58 +0100

    min

--- consumer.js
+++ consumer.js
@@ -5,17 +5,35 @@ const kafka = new Kafka({
     brokers: ['localhost:9092']
 });
 
-const consumer = kafka.consumer({ groupId: 'employee-group' })
+const consumer = kafka.consumer({ 
+    groupId: 'employee-group',
+    retry: {
+        initialRetryTime: 100,
+        retries: 0
+    }
+ })
 
 await consumer.subscribe({ topic: 'employee-topic', fromBeginning: false });
 
 await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
+    autoCommit: false,
+    eachMessage: async ({ topic, partition, message, heartbeat }) => {
         console.log({
             partition,
             offset: message.offset,
             // key: message.key.toString(),
             value: message.value.toString()
         })
-    }
+        
+        await new Promise((res) => setTimeout(res, 1000))
+        await heartbeat()
+        if (message.offset == 69) throw new Error("=>Kafka Error")
+        await consumer.commitOffsets([
+            { 
+                topic,
+                partition,
+                offset: Number(message.offset).toString()
+            }
+        ])
+    },
 })
